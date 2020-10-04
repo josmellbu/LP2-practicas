97932fa271069ec9856f6d8f63684b91a3d17584
josmellbu <josmell.quispe@upeu.edu.pe>
2020-10-04 12:26:27 -0500

    CRUD

--- Go/maingin.go
+++ Go/maingin.go
@@ -1,13 +1,104 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	//"model"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Person struct {
+    gorm.Model
+    Name string
+    Age  string
+}
 
 func main() {
+	dsn := "docker:docker@tcp(db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	// Migrate the schema
+	db.AutoMigrate(&Person{})
+
+	//s := Person{Name: "Sean", Age: 50}
+	//s.Name = "Sean"
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Holi",
+			"message": "Hola GIN",
 		})
 	})
+
+	r.GET("/persons/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var d Person
+		if err := db.First(&d, id).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+  		//db.First(&d, id)
+		c.JSON(http.StatusOK, &d)
+	})
+
+	r.GET("/persons/", func(c *gin.Context) {
+		var lis []Person
+  		db.Find(&lis)
+		c.JSON(http.StatusOK, lis)
+	})
+
+	r.POST("/persons/", func(c *gin.Context) {
+		//d := Person{Name: c.PostForm("name"), Age: c.PostForm("age")}
+		//db.Create(&d)
+		//c.JSON(200, gin.H{
+		//	"name": d.Name,
+		//	"age": d.Age,
+		//})
+		//var d Person
+		d := Person{Name: c.PostForm("name"), Age: c.PostForm("age")}
+		/*if err := c.BindJSON(&d); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}*/
+		db.Create(&d)
+		c.JSON(http.StatusOK, &d)
+	})
+
+	r.PUT("/persons/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var d Person
+		if err := db.First(&d, id).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		d.Name = c.PostForm("name")
+		d.Age = c.PostForm("age")
+  		db.Save(&d)
+		c.JSON(http.StatusOK, &d)
+	})
+
+	r.DELETE("/persons/:id", func(c *gin.Context){
+		id := c.Param("id")
+		var d Person
+		if err := db.Where("id = ?", id).First(&d).Error; err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+		db.Unscoped().Delete(&d)
+	})
+
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
