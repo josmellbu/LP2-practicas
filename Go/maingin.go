c09d81347339438518273bd2cb865917ad1d4ae8
josmellbu <josmell.quispe@upeu.edu.pe>
2020-10-13 00:17:49 -0500

    token

--- Go/maingin.go
+++ Go/maingin.go
@@ -14,6 +14,18 @@ type Person struct {
     Age  string
 }
 
+type User struct {
+	ID uint64            `json:"id"`
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+	//A sample use
+	var user = User{
+		ID:             1,
+		Username: "username",
+		Password: "password",
+	}
+
 func main() {
 	dsn := "docker:docker@tcp(db:3306)/test_db?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -29,11 +41,14 @@ func main() {
 	//s.Name = "Sean"
 
 	r := gin.Default()
+
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hola GIN",
 		})
 	})
+
+	r.GET("/login", Login)
 
 	r.GET("/persons/:id", func(c *gin.Context) {
 		id := c.Param("id")
@@ -102,3 +117,41 @@ func main() {
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+
+
+
+func Login(c *gin.Context) {
+	var u User
+	if err := c.ShouldBindJSON(&u); err != nil {
+	   c.JSON(http.StatusUnprocessableEntity, "Invalid json provided")
+	   return
+	}
+	//compare the user from the request, with the one we defined:
+	if user.Username != u.Username || user.Password != u.Password {
+	   c.JSON(http.StatusUnauthorized, "Please provide valid login details")
+	   return
+	}
+	token, err := CreateToken(user.ID)
+	if err != nil {
+	   c.JSON(http.StatusUnprocessableEntity, err.Error())
+	   return
+	}
+	c.JSON(http.StatusOK, token)
+  }
+
+  func CreateToken(userid uint64) (string, error) {
+	var err error
+	//Creating Access Token
+	os.Setenv("ACCESS_SECRET", "jdnfksdmfksd") //this should be in an env file
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["user_id"] = userid
+	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
+	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+	if err != nil {
+	   return "", err
+	}
+	return token, nil
+  }
